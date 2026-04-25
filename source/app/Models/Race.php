4dace0cb40290062c5f4ae2b7bb82d9b6b4acd5a
nakajima97 <kituneudon97@gmail.com>
2026-04-25 21:31:48 +0900

    #169 ShowResultActionでfinishing_horsesを返却する実装を追加

--- source/app/Models/Race.php
+++ source/app/Models/Race.php
@@ -43,4 +43,10 @@ class Race extends Model
     {
         return $this->hasMany(RaceEntry::class);
     }
+
+    /** @return HasMany<RaceResultHorse, $this> */
+    public function raceResultHorses(): HasMany
+    {
+        return $this->hasMany(RaceResultHorse::class);
+    }
 }
